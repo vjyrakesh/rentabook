c82c6230523106d55303efc9d1fdba403ffb690b
Rakesh Kasibhatla <rkasibha@cisco.com>
2020-03-28 19:27:41 +0530

    Initial commit

---
+++ src/test/java/com/rkasibha/rentabook/RentabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkasibha.rentabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
